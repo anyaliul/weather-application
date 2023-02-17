18bebce10580bfc64455c9ab5a82c32233215f7b
Anna Liulchak <49758623+anyaliul@users.noreply.github.com>
2023-02-16 18:58:32 -0800

    Added HTML, CSS and JS files

---
+++ src/app.js
@@ -0,0 +1,111 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let windElement = document.querySelector("#wind");
+
+  let iconElement = document.querySelector("#icon");
+  let descriptionElement = document.querySelector("#description");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function search(city) {
+  let apiKey = "7ed26a6948c661d05fafe7355b41b2ec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-field-input");
+  search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let searchForm = document.querySelector("#search-field");
+searchForm.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
+
+// function typeCity(event) {
+//   event.preventDefault();
+//   let cityInput = document.querySelector("#search-field-input");
+//   let apiKey = "7ed26a6948c661d05fafe7355b41b2ec";
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+//   axios.get(apiUrl).then(showCity);
+//   axios.get(apiUrl).then(showTemp);
+//   axios.get(apiUrl).then(showHumidity);
+//   axios.get(apiUrl).then(showFeelsLike);
+// }
+
+// function showCity(response) {
+//   let city = response.data.name;
+//   let cityName = document.querySelector("#city");
+//   cityName.innerHTML = `${city}`;
+// }
+
+// function showTemp(response) {
+//   let temperature = Math.round(response.data.main.temp);
+//   let tempInCity = document.querySelector("#temperature");
+//   tempInCity.innerHTML = `${temperature} °C`;
+// }
+
+// function showHumidity(response) {
+//   let humidity = response.data.main.humidity;
+//   let humidityInCity = document.querySelector("#humidity");
+//   humidityInCity.innerHTML = `Humidity: ${humidity}%`;
+// }
+
+// function showFeelsLike(response) {
+//   let feelsLike = Math.round(response.data.main.feels_like);
+//   let feelsLikeInCity = document.querySelector("#feels-like");
+//   feelsLikeInCity.innerHTML = `Feels like: ${feelsLike} °C`;
+// }
+
+// function showCurrentCity(response) {
+//   let currentCity = document.querySelector("#city");
+//   currentCity.innerHTML = `${response.data.name}`;
+// }
+
+// let searchForm = document.querySelector("#search-field");
+// searchForm.addEventListener("submit", typeCity);
